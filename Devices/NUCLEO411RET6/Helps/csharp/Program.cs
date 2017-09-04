45e58f460cab1de6a8eddcd89943bd884d442bac
Valon M Hoti <valon.hoti@gmail.com>
2017-09-04 11:10:05 +0200

    Updated with Nucleo STM32F411RET6 board
    is derivate of FEZCLR but just reconfigured for Nucleo STM32F411RET6 as is (included helper file and doc)

---
+++ Devices/NUCLEO411RET6/Helps/csharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using GHIElectronics.TinyCLR.Devices.Gpio;
+
+namespace BlinkTinyCLR
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var controller = GpioController.GetDefault();
+            var pin = controller.OpenPin(STM.NUCLEO.STM32F411.Arduino.Led1);                        
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+     
+            while (true)
+            {
+                pin.Write(GpioPinValue.High);
+                Thread.Sleep(500);
+
+                Debug.WriteLine("Led on ..");
+
+                pin.Write(GpioPinValue.Low);
+                Thread.Sleep(500);
+
+                Debug.WriteLine("Led off..");
+
+            }
+        }
+     }
+}
