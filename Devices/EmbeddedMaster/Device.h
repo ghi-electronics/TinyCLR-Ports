cf9077b4d4eebcca6c572f3f34e104f14a6cf94b
John Brochue <john.brochue@ghielectronics.com>
2018-02-01 10:24:53 -0500

    Updated versions

--- Devices/EmbeddedMaster/Device.h
+++ Devices/EmbeddedMaster/Device.h
@@ -17,7 +17,7 @@
 #define DEVICE_TARGET LPC24
 #define DEVICE_NAME "EmbeddedMaster"
 #define DEVICE_MANUFACTURER "GHI Electronics, LLC"
-#define DEVICE_VERSION ((0ULL << 48) | (7ULL << 32) | (0ULL << 16) | (0ULL << 0))
+#define DEVICE_VERSION ((0ULL << 48) | (8ULL << 32) | (0ULL << 16) | (0ULL << 0))
 
 #define USB_DEBUGGER_VENDOR_ID 0x1B9F
 #define USB_DEBUGGER_PRODUCT_ID 0x5001
