32aba0319ebfad04f9b60cf3cae41eb910817f65
Dat <dat.tran@ghielectronics.com>
2018-02-22 14:38:05 -0500

    Close a pin also config that pin to be input. Don't need to call twice because size optimization

--- Targets/STM32F4/STM32F4_SPI.cpp
+++ Targets/STM32F4/STM32F4_SPI.cpp
@@ -484,11 +484,7 @@ TinyCLR_Result STM32F4_Spi_Release(const TinyCLR_Spi_Provider* self) {
     auto& sclk = g_STM32F4_Spi_Sclk_Pins[controller];
     auto& miso = g_STM32F4_Spi_Miso_Pins[controller];
     auto& mosi = g_STM32F4_Spi_Mosi_Pins[controller];
-
-    STM32F4_GpioInternal_ConfigurePin(miso.number, STM32F4_Gpio_PortMode::Input, STM32F4_Gpio_OutputType::PushPull, STM32F4_Gpio_OutputSpeed::VeryHigh, STM32F4_Gpio_PullDirection::None, STM32F4_Gpio_AlternateFunction::AF0);
-    STM32F4_GpioInternal_ConfigurePin(mosi.number, STM32F4_Gpio_PortMode::Input, STM32F4_Gpio_OutputType::PushPull, STM32F4_Gpio_OutputSpeed::VeryHigh, STM32F4_Gpio_PullDirection::None, STM32F4_Gpio_AlternateFunction::AF0);
-    STM32F4_GpioInternal_ConfigurePin(sclk.number, STM32F4_Gpio_PortMode::Input, STM32F4_Gpio_OutputType::PushPull, STM32F4_Gpio_OutputSpeed::VeryHigh, STM32F4_Gpio_PullDirection::None, STM32F4_Gpio_AlternateFunction::AF0);
-
+    
     STM32F4_GpioInternal_ClosePin(sclk.number);
     STM32F4_GpioInternal_ClosePin(miso.number);
     STM32F4_GpioInternal_ClosePin(mosi.number);
