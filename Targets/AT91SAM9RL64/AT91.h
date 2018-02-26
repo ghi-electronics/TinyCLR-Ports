8614404e866a6ab8c047cfc6bdd03b1fd066d2f3
Dat <dat.tran@ghielectronics.com>
2018-02-26 12:24:26 -0500

    Do move flash driver for Hydra

--- Targets/AT91SAM9RL64/AT91.h
+++ Targets/AT91SAM9RL64/AT91.h
@@ -850,15 +850,6 @@ TinyCLR_Result AT91_Uart_SetWriteBufferSize(const TinyCLR_Uart_Provider* self, s
 
 //Deployment
 const TinyCLR_Api_Info* AT91_Deployment_GetApi();
-TinyCLR_Result AT91_Flash_Acquire(const TinyCLR_Deployment_Provider* self, bool &supportXIP);
-TinyCLR_Result AT91_Flash_Release(const TinyCLR_Deployment_Provider* self);
-TinyCLR_Result AT91_Flash_Read(const TinyCLR_Deployment_Provider* self, uint32_t address, size_t length, uint8_t* buffer);
-TinyCLR_Result AT91_Flash_Write(const TinyCLR_Deployment_Provider* self, uint32_t address, size_t length, const uint8_t* buffer);
-TinyCLR_Result AT91_Flash_EraseBlock(const TinyCLR_Deployment_Provider* self, uint32_t sector);
-TinyCLR_Result AT91_Flash_IsBlockErased(const TinyCLR_Deployment_Provider* self, uint32_t sector, bool &erased);
-TinyCLR_Result AT91_Flash_GetBytesPerSector(const TinyCLR_Deployment_Provider* self, uint32_t address, int32_t& size);
-TinyCLR_Result AT91_Flash_GetSectorMap(const TinyCLR_Deployment_Provider* self, const uint32_t*& addresses, const uint32_t*& sizes, size_t& count);
-TinyCLR_Result AT91_Flash_Reset(const TinyCLR_Deployment_Provider* self);
 
 // Interrupt
 //////////////////////////////////////////////////////////////////////////////
