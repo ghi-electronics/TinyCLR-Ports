c23273614d2b8dc649ae2acc4c9d06a98582d195
Dat <dat.tran@ghielectronics.com>
2018-02-26 12:28:23 -0500

    Update for G400 follow Hydra

--- Devices/G400/Device.cpp
+++ Devices/G400/Device.cpp
@@ -1,5 +1,12 @@
 #include "Device.h"
 
+
+#include "../../Drivers/AT45DB321D_Flash/AT45DB321D_Flash.h"
+
 void AT91_Startup_OnSoftResetDevice(const TinyCLR_Api_Provider* apiProvider) {
 
+}
+
+const TinyCLR_Api_Info* AT91_Deployment_GetApi() {
+    return AT45DB321D_Deployment_GetApi();
 }
