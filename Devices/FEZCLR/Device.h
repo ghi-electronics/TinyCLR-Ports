170f9d34382ecfe086b55143e5249415dabc4a2e
John Brochue <johnbrochue@outlook.com>
2018-04-04 14:24:18 -0400

    Updated versions.

--- Devices/FEZCLR/Device.h
+++ Devices/FEZCLR/Device.h
@@ -21,7 +21,7 @@
 #define DEVICE_TARGET STM32F4
 #define DEVICE_NAME "FEZCLR"
 #define DEVICE_MANUFACTURER "GHI Electronics, LLC"
-#define DEVICE_VERSION ((0ULL << 48) | (9ULL << 32) | (0ULL << 16) | (0ULL << 0))
+#define DEVICE_VERSION ((0ULL << 48) | (10ULL << 32) | (0ULL << 16) | (0ULL << 0))
 
 #define USB_DEBUGGER_VENDOR_ID 0x1B9F
 #define USB_DEBUGGER_PRODUCT_ID 0x500B
