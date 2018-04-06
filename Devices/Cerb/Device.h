229be32293f5e452cad5ddc237db3f4440bbe178
Dat <dat.tran@ghielectronics.com>
2018-04-06 12:48:19 -0400

    Implement RTC for Cerb

--- Devices/Cerb/Device.h
+++ Devices/Cerb/Device.h
@@ -88,6 +88,8 @@
                           /* TIM14 */ { { PIN(A,  7) , AF(9)   }, { PIN_NONE  , AF_NONE }, { PIN_NONE  , AF_NONE }, { PIN_NONE   , AF_NONE }  },\
                          }
 
+#define INCLUDE_RTC
+
 #define INCLUDE_SPI
 #define STM32F4_SPI_SCLK_PINS { { PIN(B, 3), AF(5) } }
 #define STM32F4_SPI_MISO_PINS { { PIN(B, 4), AF(5) } }
