171a9cbd3d3837fe601593951e07f280f08786fe
Dat <dat.tran@ghielectronics.com>
2018-04-30 16:22:10 -0400

    Update USBC FEZHydra from G400

--- Devices/FEZHydra/Device.cpp
+++ Devices/FEZHydra/Device.cpp
@@ -5,8 +5,4 @@
 
 void AT91_Startup_OnSoftResetDevice(const TinyCLR_Api_Provider* apiProvider) {
     apiProvider->Add(apiProvider, SPIDisplay_GetApi());
-
-#ifdef INCLUDE_USBCLIENT
-    apiProvider->Add(apiProvider, UsbClient_GetApi());
-#endif
 }
