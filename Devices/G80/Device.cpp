be51d5a4d77a0fc64a0507cfb1f695f9eca68de7
Dat <dat.tran@ghielectronics.com>
2018-05-07 14:23:44 -0400

    Add Device library from port

--- Devices/G80/Device.cpp
+++ Devices/G80/Device.cpp
@@ -4,4 +4,12 @@
 
 void STM32F4_Startup_OnSoftResetDevice(const TinyCLR_Api_Provider* apiProvider) {
     apiProvider->Add(apiProvider, SPIDisplay_GetApi());
+    
+    // extern const TinyCLR_Interop_Assembly Interop_GHIElectronics_TinyCLR_Devices;
+
+    // auto interopProvider = reinterpret_cast<const TinyCLR_Interop_Provider*>(apiProvider->FindDefault(apiProvider, TinyCLR_Api_Type::InteropProvider));
+
+    // if (interopProvider != nullptr)
+        // interopProvider->Add(interopProvider, &Interop_GHIElectronics_TinyCLR_Devices);
 }
+
