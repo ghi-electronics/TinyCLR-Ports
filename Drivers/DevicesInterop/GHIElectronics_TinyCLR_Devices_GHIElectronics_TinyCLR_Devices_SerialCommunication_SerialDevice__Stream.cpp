2f15823e425c45962a0b491c8180b63d94a04226
Dat <dat.tran@ghielectronics.com>
2018-05-08 15:05:32 -0400

    Convert Uart

--- Drivers/DevicesInterop/GHIElectronics_TinyCLR_Devices_GHIElectronics_TinyCLR_Devices_SerialCommunication_SerialDevice__Stream.cpp
+++ Drivers/DevicesInterop/GHIElectronics_TinyCLR_Devices_GHIElectronics_TinyCLR_Devices_SerialCommunication_SerialDevice__Stream.cpp
@@ -14,10 +14,6 @@ void ErrorReceivedIsr(const TinyCLR_Uart_Provider* self, TinyCLR_Uart_Error erro
         interopProvider->RaiseEvent(interopProvider, "GHIElectronics.TinyCLR.NativeEventNames.Uart.ErrorReceived", self->Parent->Name, self->Index, (uint64_t)error, 0, 0);
 }
 
-void SetUartInToDo(const TinyCLR_Uart_Provider* self, size_t count) {
-    while (1);
-}
-
 TinyCLR_Result Interop_GHIElectronics_TinyCLR_Devices_GHIElectronics_TinyCLR_Devices_SerialCommunication_SerialDevice__Stream::NativeOpen___VOID__U4__U4__U4__U4__U4(const TinyCLR_Interop_MethodData md) {
     auto provider = (const TinyCLR_Uart_Provider*)TinyCLR_Interop_GetProvider(md, FIELD___nativeProvider___I);
 
@@ -38,7 +34,6 @@ TinyCLR_Result Interop_GHIElectronics_TinyCLR_Devices_GHIElectronics_TinyCLR_Dev
             if (provider->SetActiveSettings(provider, baudRate, dataBits, (TinyCLR_Uart_Parity)parity, (TinyCLR_Uart_StopBitCount)stopBits, (TinyCLR_Uart_Handshake)handshaking) == TinyCLR_Result::Success) {
                 provider->SetPinChangedHandler(provider, PinChangedIsr);
                 provider->SetErrorReceivedHandler(provider, ErrorReceivedIsr);
-                provider->SetDataReceivedHandler(provider, SetUartInToDo);
 
                 return TinyCLR_Result::Success;
             }
@@ -69,112 +64,45 @@ TinyCLR_Result Interop_GHIElectronics_TinyCLR_Devices_GHIElectronics_TinyCLR_Dev
 TinyCLR_Result Interop_GHIElectronics_TinyCLR_Devices_GHIElectronics_TinyCLR_Devices_SerialCommunication_SerialDevice__Stream::NativeRead___I4__SZARRAY_U1__I4__I4__I4(const TinyCLR_Interop_MethodData md) {
     auto provider = (const TinyCLR_Uart_Provider*)TinyCLR_Interop_GetProvider(md, FIELD___nativeProvider___I);
 
-    auto arg1 = TinyCLR_Interop_GetArguments(md, 1);
-    auto arg2 = TinyCLR_Interop_GetArguments(md, 2);
-    auto arg3 = TinyCLR_Interop_GetArguments(md, 3);
-    auto arg4 = TinyCLR_Interop_GetArguments(md, 4);
+    auto arg1 = TinyCLR_Interop_GetArguments(md, 1); // buffer
+    auto arg2 = TinyCLR_Interop_GetArguments(md, 2); // offset
+    auto arg3 = TinyCLR_Interop_GetArguments(md, 3); // count
+    auto arg4 = TinyCLR_Interop_GetArguments(md, 4); // Timeout
 
     size_t count = arg3.Data.Numeric->U4;
 
     uint8_t* ptr = (uint8_t*)arg1.Data.SzArray.Data + arg2.Data.Numeric->I4;
 
     bool result = true;
-    int32_t total;
-    int64_t* timeoutTicks;
 
-    while (1) {};// TODO
+    provider->Read(provider, ptr, count);
 
-    //LIB_TC_CHECK_HRESULT(md.Stack.SetupTimeout(md.Stack.Arg4(), timeoutTicks));
-    //TODO
+    auto ret = TinyCLR_Interop_GetReturn(md);
 
-    // if (md.Stack.m_customState == 1) {
-        // md.Stack.PushValueI4(0);
-
-        // md.Stack.m_customState = 2;
-    // }
-
-    // total = md.Stack.m_evalStack[1].NumericByRef().s4;
-
-    // ptr += total;
-    // count -= total;
-
-    // while (result && count > 0) {
-        // size_t read = count;
-
-        // provider->Read(provider, ptr, read);
-
-        // if (read == 0) {
-            // md.Stack.m_evalStack[1].NumericByRef().s4 = total;
-
-            // LIB_TC_CHECK_HRESULT(g_CLR_RT_ExecutionEngine.WaitEvents(md.Stack.m_owningThread, *timeoutTicks, CLR_RT_ExecutionEngine::c_Event_SerialPort, result));
-        // }
-        // else if (read < 0) {
-            // LIB_TC_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
-        // }
-        // else {
-            // ptr += read;
-            // total += read;
-            // count -= read;
-
-            // break;
-        // }
-    // }
-
-    // md.Stack.PopValue();
-    // md.Stack.PopValue();
-
-    // md.Stack.SetResult_I4(total);
+    ret.Data.Numeric->I4 = (int32_t)count;
 
     return TinyCLR_Result::Success;
 }
 
 TinyCLR_Result Interop_GHIElectronics_TinyCLR_Devices_GHIElectronics_TinyCLR_Devices_SerialCommunication_SerialDevice__Stream::NativeWrite___I4__SZARRAY_U1__I4__I4__I4(const TinyCLR_Interop_MethodData md) {
-    while (1) {};// TODO
-   // auto provider = (const TinyCLR_Uart_Provider*)md.Stack.This()[FIELD___nativeProvider___I].NumericByRefConst().s4;
+    auto provider = (const TinyCLR_Uart_Provider*)TinyCLR_Interop_GetProvider(md, FIELD___nativeProvider___I);
 
-   // size_t count = md.Stack.Arg3().NumericByRef().s4;
-   // uint8_t* ptr = (uint8_t*)md.Stack.Arg1().DereferenceArray()->GetFirstElement() + md.Stack.Arg2().NumericByRef().s4;
-   // bool result = true;
-   // int32_t total;
-   // int64_t* timeoutTicks;
+    auto arg1 = TinyCLR_Interop_GetArguments(md, 1); // buffer
+    auto arg2 = TinyCLR_Interop_GetArguments(md, 2); // offset
+    auto arg3 = TinyCLR_Interop_GetArguments(md, 3); // count
+    auto arg4 = TinyCLR_Interop_GetArguments(md, 4); // Timeout
 
-   // LIB_TC_CHECK_HRESULT(md.Stack.SetupTimeout(md.Stack.Arg4(), timeoutTicks));
+    size_t count = arg3.Data.Numeric->U4;
 
-   // if (md.Stack.m_customState == 1) {
-       // md.Stack.PushValueI4(0);
+    uint8_t* ptr = (uint8_t*)arg1.Data.SzArray.Data + arg2.Data.Numeric->I4;
 
-       // md.Stack.m_customState = 2;
-   // }
+    bool result = true;
 
-   // total = md.Stack.m_evalStack[1].NumericByRef().s4;
+    provider->Write(provider, ptr, count);
 
-   // ptr += total;
-   // count -= total;
+    auto ret = TinyCLR_Interop_GetReturn(md);
 
-   // while (result && count > 0) {
-       // size_t write = count;
-
-       // provider->Write(provider, ptr, write);
-
-       // if (write == 0) {
-           // md.Stack.m_evalStack[1].NumericByRef().s4 = total;
-
-           // LIB_TC_CHECK_HRESULT(g_CLR_RT_ExecutionEngine.WaitEvents(md.Stack.m_owningThread, *timeoutTicks, CLR_RT_ExecutionEngine::c_Event_SerialPort, result));
-       // }
-       // else if (write < 0) {
-           // LIB_TC_SET_AND_LEAVE(CLR_E_INVALID_PARAMETER);
-       // }
-       // else {
-           // ptr += write;
-           // total += write;
-           // count -= write;
-       // }
-   // }
-
-   // md.Stack.PopValue();
-   // md.Stack.PopValue();
-
-   // md.Stack.SetResult_I4(total);
+    ret.Data.Numeric->I4 = (int32_t)count;
 
     return TinyCLR_Result::Success;
 }
