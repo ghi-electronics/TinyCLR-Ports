9438c74fb171e72eba21b8302ec63b70c7c27f1e
Dat <dat.tran@ghielectronics.com>
2018-05-09 08:53:59 -0400

    Add build device assembly to other builds

--- Devices/USBizi/Device.cpp
+++ Devices/USBizi/Device.cpp
@@ -19,6 +19,13 @@
 
 void LPC24_Startup_OnSoftResetDevice(const TinyCLR_Api_Provider* apiProvider) {
     apiProvider->Add(apiProvider, SPIDisplay_GetApi());
+    
+    extern const TinyCLR_Interop_Assembly Interop_GHIElectronics_TinyCLR_Devices;
+
+    auto interopProvider = reinterpret_cast<const TinyCLR_Interop_Provider*>(apiProvider->FindDefault(apiProvider, TinyCLR_Api_Type::InteropProvider));
+
+    if (interopProvider != nullptr)
+        interopProvider->Add(interopProvider, &Interop_GHIElectronics_TinyCLR_Devices);
 }
 
 static int32_t lpc24_deviceId = 0;
