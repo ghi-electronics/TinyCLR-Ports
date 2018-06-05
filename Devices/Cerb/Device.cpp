ff1ff83a9b403d8574b77ac7a3fa16e91df80329
John Brochue <john.brochue@ghielectronics.com>
2018-06-05 13:08:01 -0400

    Fixed Cerb style to match the rest

--- Devices/Cerb/Device.cpp
+++ Devices/Cerb/Device.cpp
@@ -4,10 +4,6 @@
 #include "../../Drivers/DevicesInterop/GHIElectronics_TinyCLR_Devices_GHIElectronics_TinyCLR_Devices_Interop.h"
 
 void STM32F4_Startup_OnSoftResetDevice(const TinyCLR_Api_Provider* apiProvider, const TinyCLR_Interop_Provider* interopProvider) {
-    apiProvider->Add(apiProvider, SPIDisplay_GetApi());    
-
-    auto interopProvider = reinterpret_cast<const TinyCLR_Interop_Provider*>(apiProvider->FindDefault(apiProvider, TinyCLR_Api_Type::InteropProvider));
-
-    if (interopProvider != nullptr)
-        interopProvider->Add(interopProvider, &Interop_GHIElectronics_TinyCLR_Devices);
+    apiProvider->Add(apiProvider, SPIDisplay_GetApi());
+    interopProvider->Add(interopProvider, &Interop_GHIElectronics_TinyCLR_Devices);
 }
