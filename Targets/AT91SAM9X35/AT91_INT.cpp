d1efd88487e42373ed29f2f4bb6aade3c36aaea7
Dat <dat.tran@ghielectronics.com>
2018-07-18 13:53:42 -0400

    tweak

--- Targets/AT91SAM9X35/AT91_INT.cpp
+++ Targets/AT91SAM9X35/AT91_INT.cpp
@@ -2,7 +2,6 @@
 // Copyright GHI Electronics, LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
