33f81731297f77d826df5cbd57c9194da9984065
Dat <dat.tran@ghielectronics.com>
2018-07-24 11:32:18 -0400

    Remove unnecessary stuffs

--- Drivers/DevicesInterop/GHIElectronics_TinyCLR_InteropUtil.h
+++ Drivers/DevicesInterop/GHIElectronics_TinyCLR_InteropUtil.h
@@ -3,18 +3,8 @@
 #include <TinyCLR.h>
 #include <Device.h>
 
-extern const TinyCLR_Interop_Assembly Interop_GHIElectronics_TinyCLR_Devices;
-extern const TinyCLR_Api_Manager* apiManager;
-
 const void* TinyCLR_Interop_GetArgument(const TinyCLR_Interop_MethodData md, size_t fieldId);
 TinyCLR_Interop_ClrValue TinyCLR_Interop_GetReturn(const TinyCLR_Interop_MethodData md);
 TinyCLR_Interop_ClrValue TinyCLR_Interop_GetArguments(const TinyCLR_Interop_MethodData md, uint32_t argIndex);
 TinyCLR_Interop_ClrValue TinyCLR_Interop_GetFieldSelf(const TinyCLR_Interop_MethodData md, size_t fieldId);
 TinyCLR_Interop_ClrValue TinyCLR_Interop_GetField(const TinyCLR_Interop_MethodData md, const TinyCLR_Interop_ClrObject* self, size_t fieldId);
-
-uint64_t TinyCLR_Interop_CurrentTime();
-void TinyCLR_Interop_Delay(uint32_t microseconds);
-
-void TinyCLR_Interop_EnableInterrupt();
-void TinyCLR_Interop_DisableInterrupt();
-bool TinyCLR_Interop_GetStateInterrupt();
