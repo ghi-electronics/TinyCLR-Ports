fa914c63b6d90c58fded96bef3dc737e05bfd20c
Dat <dat.tran@ghielectronics.com>
2018-08-02 12:32:54 -0400

    F7: Fixed ADC didn't work

--- Targets/STM32F7xx/STM32F7_ADC.cpp
+++ Targets/STM32F7xx/STM32F7_ADC.cpp
@@ -31,6 +31,10 @@ static const uint32_t adcPins[] = STM32F7_ADC_PINS;
 static TinyCLR_Adc_Controller adcControllers[TOTAL_ADC_CONTROLLERS];
 static TinyCLR_Api_Info adcApi[TOTAL_ADC_CONTROLLERS];
 
+const char* adcApiNames[TOTAL_ADC_CONTROLLERS] = {
+    "GHIElectronics.TinyCLR.NativeApis.STM32F7.AdcController\\0"
+};
+
 struct AdcState {
     bool isOpen[STM32F7_AD_NUM];
 };
@@ -54,14 +58,16 @@ void STM32F7_Adc_AddApi(const TinyCLR_Api_Manager* apiManager) {
         adcControllers[i].GetChannelCount = &STM32F7_Adc_GetChannelCount;
 
         adcApi[i].Author = "GHI Electronics, LLC";
-        adcApi[i].Name = "GHIElectronics.TinyCLR.NativeApis.STM32F7.AdcController";
+        adcApi[i].Name = adcApiNames[i];
         adcApi[i].Type = TinyCLR_Api_Type::AdcController;
         adcApi[i].Version = 0;
         adcApi[i].Implementation = &adcControllers[i];
         adcApi[i].State = &adcStates[i];
+
+        apiManager->Add(apiManager, &adcApi[i]);
     }
 
-    
+    apiManager->SetDefaultName(apiManager, TinyCLR_Api_Type::AdcController, adcApi[0].Name);
 }
 
 TinyCLR_Result STM32F7_Adc_Acquire(const TinyCLR_Adc_Controller* self) {
