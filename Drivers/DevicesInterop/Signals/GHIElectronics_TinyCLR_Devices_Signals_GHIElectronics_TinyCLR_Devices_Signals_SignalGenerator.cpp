c9850121328ce97475e03e18ce6be58d75513ee6
John Brochue <johnbrochue@outlook.com>
2018-08-09 09:29:34 -0400

    Respect offset and count

--- Drivers/DevicesInterop/Signals/GHIElectronics_TinyCLR_Devices_Signals_GHIElectronics_TinyCLR_Devices_Signals_SignalGenerator.cpp
+++ Drivers/DevicesInterop/Signals/GHIElectronics_TinyCLR_Devices_Signals_GHIElectronics_TinyCLR_Devices_Signals_SignalGenerator.cpp
@@ -1,17 +1,19 @@
 #include "GHIElectronics_TinyCLR_Devices_Signals.h"
 
 TinyCLR_Result Interop_GHIElectronics_TinyCLR_Devices_Signals_GHIElectronics_TinyCLR_Devices_Signals_SignalGenerator::Write___VOID__SZARRAY_I8__I4__I4(const TinyCLR_Interop_MethodData md) {
-    TinyCLR_Interop_ClrValue arrArg, apiFld, pinFld;
+    TinyCLR_Interop_ClrValue arrArg, offsetArg, countArg, apiFld, pinFld;
     const TinyCLR_Interop_ClrObject* self;
 
     md.InteropManager->GetArgument(md.InteropManager, md.Stack, 0, arrArg);
+    md.InteropManager->GetArgument(md.InteropManager, md.Stack, 1, offsetArg);
+    md.InteropManager->GetArgument(md.InteropManager, md.Stack, 2, countArg);
     md.InteropManager->GetThisObject(md.InteropManager, md.Stack, self);
     md.InteropManager->GetField(md.InteropManager, self, Interop_GHIElectronics_TinyCLR_Devices_Signals_GHIElectronics_TinyCLR_Devices_Signals_SignalGenerator::FIELD___gpioApi___I, apiFld);
     md.InteropManager->GetField(md.InteropManager, self, Interop_GHIElectronics_TinyCLR_Devices_Signals_GHIElectronics_TinyCLR_Devices_Signals_SignalGenerator::FIELD___pinNumber___I4, pinFld);
 
     auto time = reinterpret_cast<const TinyCLR_NativeTime_Controller*>(md.ApiManager->FindDefault(md.ApiManager, TinyCLR_Api_Type::NativeTimeController));
-    auto arr = reinterpret_cast<int64_t*>(arrArg.Data.SzArray.Data);
-    auto len = arrArg.Data.SzArray.Length;
+    auto arr = reinterpret_cast<int64_t*>(arrArg.Data.SzArray.Data) + offsetArg.Data.Numeric->I4;
+    auto len = countArg.Data.Numeric->I4;
     auto gpio = reinterpret_cast<const TinyCLR_Gpio_Controller*>(apiFld.Data.Numeric->I);
     auto pin = static_cast<uint32_t>(pinFld.Data.Numeric->I4);
     auto next = TinyCLR_Gpio_PinValue::Low;
