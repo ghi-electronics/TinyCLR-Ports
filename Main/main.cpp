2725183cadd35084ae52f5118dca57e71a72c70a
John Brochue <johnbrochue@outlook.com>
2018-09-20 10:44:19 -0400

    Call SetMemoryProfile

--- Main/main.cpp
+++ Main/main.cpp
@@ -90,7 +90,7 @@ int main() {
 
     TARGET(_Startup_GetHeap)(heapStart, heapLength);
     TinyCLR_Startup_AddHeapRegion(heapStart, heapLength);
-
+    TinyCLR_Startup_SetMemoryProfile(DEVICE_MEMORY_PROFILE_FACTOR);
 
     const TinyCLR_Api_Info *debuggerApi, *deploymentApi;
     const void* debuggerConfiguration;
