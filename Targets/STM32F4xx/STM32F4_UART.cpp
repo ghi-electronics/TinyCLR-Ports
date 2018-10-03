b28767d8bb1027ce027dc465c4afb5855563f68a
Dat <dat.tran@ghielectronics.com>
2018-10-03 13:44:40 -0400

    Enable CTS interrupt, clear cts flag asap

--- Targets/STM32F4xx/STM32F4_UART.cpp
+++ Targets/STM32F4xx/STM32F4_UART.cpp
@@ -332,10 +332,10 @@ void STM32F4_Uart_InterruptHandler(int8_t controllerIndex) {
     if (state->handshaking && (sr & USART_SR_CTS)) {
         bool ctsActive;
 
-        STM32F4_Uart_GetClearToSendState(state->controller, ctsActive);
+        // STM32F4 write 0 to clear CTS interrupt
+        (state->portReg->SR) &= ~USART_SR_CTS;
 
-        // Clear CTS interrupt
-        (state->portReg->SR) |= USART_SR_CTS;
+        STM32F4_Uart_GetClearToSendState(state->controller, ctsActive);
 
         if (canPostEvent && state->cleartosendEventHandler != nullptr)
             state->cleartosendEventHandler(state->controller, ctsActive, STM32F4_Time_GetCurrentProcessorTime());
@@ -520,7 +520,7 @@ TinyCLR_Result STM32F4_Uart_SetActiveSettings(const TinyCLR_Uart_Controller* sel
 
     switch (handshaking) {
     case TinyCLR_Uart_Handshake::RequestToSend:
-        ctrl_cr3 = USART_CR3_CTSE | USART_CR3_RTSE;
+        ctrl_cr3 = USART_CR3_CTSE | USART_CR3_RTSE | USART_CR3_CTSIE;
 
         state->handshaking = true;
         break;
