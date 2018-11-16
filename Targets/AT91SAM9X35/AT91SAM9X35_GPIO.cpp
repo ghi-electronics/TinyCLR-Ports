05098b439b1d56c108ee20089bf437896766fbfb
Dat <dat.tran@ghielectronics.com>
2018-11-16 08:13:57 -0500

    Fixed #546.

--- Targets/AT91SAM9X35/AT91SAM9X35_GPIO.cpp
+++ Targets/AT91SAM9X35/AT91SAM9X35_GPIO.cpp
@@ -161,8 +161,18 @@ void AT91SAM9X35_Gpio_InterruptHandler(void* param) {
                     interruptState->lastDebounceTicks = AT91SAM9X35_Time_GetTimeForProcessorTicks(nullptr, AT91SAM9X35_Time_GetCurrentProcessorTicks(nullptr));
                 }
 
-                if (executeIsr)
-                    interruptState->handler(interruptState->controller, interruptState->pin, edge, AT91SAM9X35_Time_GetCurrentProcessorTime());
+                if (executeIsr) {
+                    auto st = reinterpret_cast<const TinyCLR_SystemTime_Manager*>(apiManager->FindDefault(apiManager, TinyCLR_Api_Type::SystemTimeManager));
+
+                    uint64_t utc;
+                    int32_t tz;
+
+                    //To scale to the current internal epoch of 1601-01-01
+                    const uint64_t TICKS_AT_ORIGIN = 504911232000000000;
+                    auto res = st->GetTime(st, utc, tz);
+
+                    interruptState->handler(interruptState->controller, interruptState->pin, edge, utc - TICKS_AT_ORIGIN);
+                }
             }
 
             interruptsActive ^= bitMask;
