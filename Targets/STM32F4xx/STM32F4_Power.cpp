7e2e487fa66d21ee462d025f682b9d23aa1163d0
Dat <dat.tran@ghielectronics.com>
2018-12-19 11:29:57 -0500

    STM32F4: Implement deep sleep and power off

--- Targets/STM32F4xx/STM32F4_Power.cpp
+++ Targets/STM32F4xx/STM32F4_Power.cpp
@@ -17,6 +17,11 @@
 #include "STM32F4.h"
 
 #define TOTAL_POWER_CONTROLLERS 1
+/* CR register bit mask */
+#define CR_DS_MASK               ((uint32_t)0xFFFFFFFC)
+#define CR_PLS_MASK              ((uint32_t)0xFFFFFF1F)
+
+extern "C" void SystemInit();
 
 struct PowerState {
     uint32_t controllerIndex;
@@ -71,14 +76,57 @@ void STM32F4_Power_AddApi(const TinyCLR_Api_Manager* apiManager) {
 }
 
 TinyCLR_Result STM32F4_Power_SetLevel(const TinyCLR_Power_Controller* self, TinyCLR_Power_Level level, TinyCLR_Power_WakeSource wakeSource, uint64_t data) {
+    volatile uint32_t tmpreg = 0;
+
     switch (level) {
     case TinyCLR_Power_Level::Sleep1: // Sleep
     case TinyCLR_Power_Level::Sleep2: // Sleep
     case TinyCLR_Power_Level::Sleep3: // Sleep
-    case TinyCLR_Power_Level::Off:    // Off
-    case TinyCLR_Power_Level::Custom: // Custom
-        //TODO
-        return TinyCLR_Result::NotSupported;
+
+        TinyCLR_UsbClient_Uninitialize(nullptr);
+
+        RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+
+        /* Select the regulator state in STOP mode ---------------------------------*/
+        tmpreg = PWR->CR;
+        /* Clear PDDS and LPDSR bits */
+        tmpreg &= CR_DS_MASK;
+
+        /* Set LPDSR bit according to PWR_Regulator value */
+        tmpreg |= PWR_CR_LPDS;
+
+        /* Store the new value */
+        PWR->CR = tmpreg;
+
+        /* Set SLEEPDEEP bit of Cortex System Control Register */
+        SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+
+        __WFI();
+
+        /* Reset SLEEPDEEP bit of Cortex System Control Register */
+        SCB->SCR &= (uint32_t)~((uint32_t)SCB_SCR_SLEEPDEEP_Msk);
+
+        SystemInit();
+
+        TinyCLR_UsbClient_Initialize(nullptr);
+
+        break;
+
+    case TinyCLR_Power_Level::Off:// Off - Non wakeup
+
+        /* Clear Wakeup flag */
+        PWR->CR |= PWR_CR_CWUF;
+
+        /* Select STANDBY mode */
+        PWR->CR |= PWR_CR_PDDS;
+
+        /* Set SLEEPDEEP bit of Cortex System Control Register */
+        SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+
+        /* Request Wait For Interrupt */
+        __WFI();
+
+        break;
 
     case TinyCLR_Power_Level::Idle:   // Idle
         PWR->CR |= PWR_CR_CWUF;
@@ -86,11 +134,16 @@ TinyCLR_Result STM32F4_Power_SetLevel(const TinyCLR_Power_Controller* self, Tiny
         __WFI(); // sleep and wait for interrupt
         return TinyCLR_Result::Success;
 
-    case TinyCLR_Power_Level::Active: // Active
+    case TinyCLR_Power_Level::Custom: // Custom - NotSupported
+        //TODO
+        return TinyCLR_Result::NotSupported;
+
+    case TinyCLR_Power_Level::Active: // Active - Highest performance
     default:
-        // Highest performance
         return TinyCLR_Result::Success;
     }
+
+    return TinyCLR_Result::Success;
 }
 
 TinyCLR_Result STM32F4_Power_Reset(const TinyCLR_Power_Controller* self, bool runCoreAfter) {
